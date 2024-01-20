a92ee0272ea0523301a09e2fa183c027265f3368
jose noboa <j.noboa@outlook.com>
2024-01-20 14:54:23 -0500

    hw

---
+++ code_challenges/code_challenges.rb
@@ -0,0 +1,21 @@
+# challenge 2
+good_driving_record = true
+age = 25
+if good_driving_record == true && age > 25
+    puts "they should get a discount on the car rental"
+elsif   good_driving_record == true || age > 25
+    puts "they should pay full price" 
+else good_driving_record == false && age <= 25 
+    puts "they need to have someone else sign for the rental"
+end
+
+# challenge 1 odds or even
+number = 7
+if number.odd?
+    puts "odd"
+else puts "even"
+end
+
+
+# i had to google how to do challenge 1
+# i was trying to think of math formulas that would show if the number was a float. but i couldnt think of anything off top of my head
